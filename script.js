b355de33b0d0dece48c684dd6c2869493933acb1
Anubhab <94695669+therandomuser03@users.noreply.github.com>
2023-03-09 00:05:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function imgSlider(anything) {
+    document.querySelector('.coke').src = anything;
+}
 
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
